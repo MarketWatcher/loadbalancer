8ff9914997db2a66b1928625c756f49f6f2e3993
Cengiz Can <cengizc@gmail.com>
2016-06-23 11:37:01 +0300

    T3 @kavasoglu delegate exit status of inner command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,28 @@ ecs-cli configure \
 	--compose-project-name-prefix "" \
 	--compose-service-name-prefix ""
 
+CONFIGURE_RESULT=$?
+if [ $CONFIGURE_RESULT -ne 0 ]; then
+	echo "Could not configure ECS CLI"
+	exit $CONFIGURE_RESULT
+fi
+
 # ecs-cli up --keypair marketwatcher --capability-iam --size 1 --instance-type t2.medium
 
-ecs-cli compose --file docker-compose.yml service down
-
-sleep 10
+#ecs-cli compose --file docker-compose.yml service down
+#DOWN_RESULT=$?
+#if [ $DOWN_RESULT -ne 0 ]; then
+#	echo "Could not bring service DOWN in ECS"
+#	exit $DOWN_RESULT
+#fi
+#sleep 10
 
 ecs-cli compose --file docker-compose.yml service up
+
+UP_RESULT=$?
+if [ $UP_RESULT -ne 0 ]; then
+	echo "Could not bring service UP in ECS"
+	exit $UP_RESULT
+fi
 
 cd $OLDPWD
